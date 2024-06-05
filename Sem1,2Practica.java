a1c9c94df99fff4301081b0821d5c5412cd31f1b
estefanyAzofeifa <estefanyazofeifa4@gmail.com>
2024-06-05 15:23:23 -0600

    Add files via upload
    prueba de envió de archivo

---
+++ Sem1,2Practica.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sem1.pkg2practica;
+
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author estef
+ */
+public class Sem12Practica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public class OperacionesApp {
+ 
+    public static void main(String[] args) 
+    {
+    }
+   /*
+   int a = 1;{
+   int b = 2;
+   int c = 3;
+   int d = 4;
+   System.out.println("el resultado de la suma es "+(a+b+c+d));
+   System.out.println("el resultado del promedio es "+((a+b+c+d)//4));
+    }
+   
+    //******************************************************88
+   /*
+    String texto1=JOptionPane.showInputDialog("Introduce el primero numero");
+    int num1=Integer.parseInt(texto1);
+     
+    String texto2=JOptionPane.showInputDialog("Introduce el segundo numero");
+    int num2=Integer.parseInt(texto2);
+     
+    String texto3=JOptionPane.showInputDialog("Introduce el tercer numero");
+    int num3=Integer.parseInt(texto3);
+     
+    String texto4=JOptionPane.showInputDialog("Introduce el caurto numero");
+    int num4=Integer.parseInt(texto4);
+    
+    System.out.println("el resultado de la suma es "+(num1+num2+num3+num4));
+    System.out.println("el resultado del promedio es "+((num1+num2+num3+num4)//4));
+   //******************************************************************** 
+    
+    String nombre = "";
+    nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+    System. out.println("buenos dias "+(nombre)+"bienvenida a este programa")
+    */
+    //**************************************************************
+    int aniosAntiguedad = '';
+    aniosAntiguedad = JOptionPane.showInputDialog("Ingrese los anios de antiguedad");
+    
+    horaLboradas = JOptionPane.showInputDialog("Ingrese las horas laboradas");
+    
+    precioPorHoras = JOptionPane.showInputDialog("Ingrese su salario por hora");
+    
+    salarioBruto = horaLboradas*precioPorHoras;
+     
+    if (aniosAntiguedad<10);
+    salarioBruto = salarioBruto*0,20;
+      if (salarioBruto<1000)
+      salarioBruto = salarioBruto-0,10
+      else (salarioBruto <2000)
+      salarioBruto = salarioBruto - 0,15
+      
+    System.out.println("el salario bruto es"+ (salarioBruto))
+    
